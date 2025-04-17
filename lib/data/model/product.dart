905f2712c490acdeb4a8598081213569f5b3a231
chabugon <qnrhs0913@gmail.com>
2025-04-17 19:44:02 +0900

    [feat]메인화면 UI 구성, 더미 데이터 연결

---
+++ lib/data/model/product.dart
@@ -0,0 +1,38 @@
+//번호,이미지 파일명,상품명,상품 소개,판매자,가격,주소,좋아요,채팅
+class Product {
+  final int id;
+  final String imageFileName;
+  final String title;
+  final String description;
+  final String seller;
+  final int price;
+  final String address;
+  final int likes;
+  final int chats;
+
+  Product({
+    required this.id,
+    required this.imageFileName,
+    required this.title,
+    required this.description,
+    required this.seller,
+    required this.price,
+    required this.address,
+    required this.likes,
+    required this.chats,
+  });
+
+  factory Product.fromCsv(List<dynamic> csvRow) {
+    return Product(
+      id: int.parse(csvRow[0].toString()),
+      imageFileName: csvRow[1].toString(),
+      title: csvRow[2].toString(),
+      description: csvRow[3].toString(),
+      seller: csvRow[4].toString(),
+      price: int.parse(csvRow[5].toString()),
+      address: csvRow[6].toString(),
+      likes: int.parse(csvRow[7].toString()),
+      chats: int.parse(csvRow[8].toString()),
+    );
+  }
+}
---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:apple_market/ui/home/home_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomePage extends ConsumerStatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(homeViewModel);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("르탄동"),
+        centerTitle: false,
+        actions: [Icon(Icons.notifications_none)],
+      ),
+      body: ListView.builder(
+        itemCount: state.length,
+        itemBuilder: (BuildContext context, int index) {
+          final result = state[index];
+          return Padding(
+            padding: const EdgeInsets.all(16),
+            child: Container(
+              height: 120,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withValues(),
+                      blurRadius: 5.0,
+                      spreadRadius: 0.0,
+                      offset: const Offset(2, 2),
+                    )
+                  ]),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                        "assets/sample_image/${result.imageFileName}.png"),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            result.title,
+                            maxLines: 1,
+                            overflow: TextOverflow.clip,
+                          ),
+                          Text(result.address),
+                          Text(result.price.toString()),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Icon(Icons.forum),
+                              SizedBox(width: 2),
+                              Text(result.chats.toString()),
+                              SizedBox(width: 10),
+                              Icon(Icons.favorite_border),
+                              SizedBox(width: 2),
+                              Text(result.likes.toString()),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
